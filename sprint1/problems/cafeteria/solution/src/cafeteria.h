4f19fe7683b8d71861e8f2ab4e5911314ad4cdfc
Artur <Mouter91@gmail.com>
2025-03-24 15:47:25 +0300

    24.03.25

--- sprint1/problems/cafeteria/solution/src/cafeteria.h
+++ sprint1/problems/cafeteria/solution/src/cafeteria.h
@@ -15,6 +15,7 @@
 
 namespace net = boost::asio;
 
+
 // Функция-обработчик операции приготовления хот-дога
 using HotDogHandler = std::function<void(Result<HotDog> hot_dog)>;
 
@@ -28,36 +29,43 @@ public:
     // Асинхронно готовит хот-дог и вызывает handler, как только хот-дог будет готов.
     // Этот метод может быть вызван из произвольного потока
     void OrderHotDog(HotDogHandler handler) {
-       auto sausage = store_.GetSausage();
-       auto bread = store_.GetBread();
+        auto sausage = store_.GetSausage();
+        auto bread = store_.GetBread();
 
-       auto check_ready = [this, bread, sausage]() {
-           if (bread->IsCooked() && sausage->IsCooked()) {
-               try {
-                   net::dispatch(io_, [hotdog = std::make_shared<HotDog>(++id_counter, sausage, bread)] (){});
-               } catch (const std::runtime_error e) {
-                   throw e;
-               }
-           }
-       };
+        auto check_ready = [this, bread, sausage, handler]() {
+            if (bread->IsCooked() && sausage->IsCooked()) {
+                try {
+                    auto hotdog = std::make_shared<HotDog>(++id_counter, sausage, bread);
+                    net::dispatch(io_, [handler, hotdog]() {
+                        handler(Result<HotDog>(*hotdog));  // Успех
+                    });
+                } catch (...) {
+                    net::dispatch(io_, [handler]() {
+                        handler(Result<HotDog>::FromCurrentException());  // Ошибка
+                    });
+                }
+            }
+        };
 
-       net::post(io_, [this, sausage, check_ready]() {
-               sausage->StartFry(*gas_cooker_, [&, sausage]() {
-                       sausage->StopFry();
-                       check_ready();
-                       });
-               });
+        net::post(io_, [this, sausage, check_ready]() {
+            sausage->StartFry(*gas_cooker_, [&, sausage]() {
+                sausage->StopFry();
+                check_ready();
+            });
+        });
 
-       net::post(io_, [this, bread, check_ready]() {
-               bread->StartBake(*gas_cooker_, [&, bread]() {
-                       bread->StopBaking();
-                       check_ready();
-                       });
-               });
+        net::post(io_, [this, bread, check_ready]() {
+            bread->StartBake(*gas_cooker_, [&, bread]() {
+                bread->StopBaking();
+                check_ready();
+            });
+        });
     }
 
+
+
 private:
-    int id_counter = 0;
+    std::atomic<int> id_counter = 0;
     net::io_context& io_;
     // Используется для создания ингредиентов хот-дога
     Store store_;
