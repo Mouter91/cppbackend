c4630fc22822e2e25b577683d8466fc6222a35ab
Artur <Mouter91@gmail.com>
2025-03-30 11:33:21 +0300

    30.03.25 All

--- sprint1/problems/map_json/solution/src/request_handler.h
+++ sprint1/problems/map_json/solution/src/request_handler.h
@@ -1,7 +1,13 @@
 #pragma once
+#include <boost/json/serialize.hpp>
+#include <boost/json/serializer.hpp>
+#define BOOST_BEAST_USE_STD_STRING_VIEW
+
+#include <boost/json.hpp>
+
 #include "http_server.h"
 #include "model.h"
-#include <boost/json.hpp>
+#include "tagged.h"
 #include <string_view>
 #include <utility>
 
@@ -9,6 +15,14 @@ namespace http_handler {
 namespace beast = boost::beast;
 namespace http = beast::http;
 namespace json = boost::json;
+
+using StringResponse = http::response<http::string_body>;
+
+struct ContentType {
+    ContentType() = delete;
+    constexpr static std::string_view TEXT_HTML = "text/html";
+    constexpr static std::string_view APP_JSON = "application/json";
+};
 
 class RequestHandler {
 public:
@@ -21,114 +35,129 @@ public:
 
     template <typename Body, typename Allocator, typename Send>
     void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
-        if (req.method() != http::verb::get) {
-            SendBadRequest(req, std::forward<Send>(send), "Invalid method");
-            return;
-        }
-
-        if (req.target().starts_with("/api/v1/maps")) {
-            if (req.target() == "/api/v1/maps") {
-                SendMapsList(req, std::forward<Send>(send));
-                return;
+        if (req.method() == http::verb::get) {
+            std::string target(req.target());
+            if (target == "/api/v1/maps") {
+                handlerGetMaps(std::move(req), std::forward<Send>(send));
+            } else if(target.starts_with("/api/v1/maps/")){
+                const auto mapId = model::Map::Id(std::string(target.substr(13)));
+                handlerGetMapsById(std::move(req), std::forward<Send>(send), mapId);
+            } else if (target.starts_with("/api/")) {
+                send(createErrorResponse(http::status::bad_request, "badRequest", "Bad request"));
+            } else {
+                send(createErrorResponse(http::status::not_found,"mapNotFound", "Not Found"));
             }
-
-            constexpr std::string_view prefix = "/api/v1/maps/";
-            if (req.target().starts_with(prefix)) {
-                std::string map_id(req.target.substr(prefix.size()));
-                SendMapDescription(req, std::forward<Send>(send), map_id);
-                return;
-            }
+        } else {
+            send(createErrorResponse(http::status::method_not_allowed, "notAllowed", "Method Not Allowed"));
         }
-
-        SendBadRequest(req, std::forward<Send>(send), "Bad request");
     }
 
 private:
     model::Game& game_;
 
-       template <typename Body, typename Allocator, typename Send>
-    void SendMapsList(const http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
-        json::array maps_json;
+    template <typename Body, typename Allocator, typename Send>
+    void handlerGetMaps(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+        json::array mapsJson;
+
         for (const auto& map : game_.GetMaps()) {
-            maps_json.push_back({
-                {"id", map.GetId()},
+            mapsJson.push_back({
+                {"id", *map.GetId()},
                 {"name", map.GetName()}
             });
         }
+        std::string json_str = json::serialize(mapsJson);
 
-        http::response<http::string_body> res{http::status::ok, req.version()};
-        res.set(http::field::content_type, "application/json");
-        res.body() = json::serialize(maps_json);
-        res.prepare_payload();
-        send(std::move(res));
-    }
+        send(std::move(MakeStringResponse(http::status::ok, json_str,
+                                          req.version(), req.keep_alive(), ContentType::APP_JSON)));
+    } 
+   
 
-    // Отправляет описание карты в JSON
     template <typename Body, typename Allocator, typename Send>
-    void SendMapDescription(
-        const http::request<Body, http::basic_fields<Allocator>>& req,
-        Send&& send,
-        const std::string& map_id
-    ) {
-        const auto* map = game_.FindMap(model::Map::Id(map_id));
+    void handlerGetMapsById(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send, const model::Map::Id& mapId) {
+        const auto* map = game_.FindMap(mapId);
         if (!map) {
-            SendNotFound(req, std::forward<Send>(send), "Map not found");
+            send(createErrorResponse(http::status::not_found, "mapNotFound", "Map not found"));
             return;
         }
 
-        // Формируем JSON (примерно как в задании)
-        json::value map_json = {
-            {"id", map->GetId()},
-            {"name", map->GetName()},
-            {"roads", json::array()},  // TODO: добавить дороги
-            {"buildings", json::array()},  // TODO: добавить здания
-            {"offices", json::array()}  // TODO: добавить офисы
+        json::object mapJson{
+            {"id", *map->GetId()},
+            {"name", map->GetName()}
         };
 
-        http::response<http::string_body> res{http::status::ok, req.version()};
-        res.set(http::field::content_type, "application/json");
-        res.body() = json::serialize(map_json);
-        res.prepare_payload();
-        send(std::move(res));
+        // Сериализация дорог
+        json::array roadsJson;
+        for (const auto& road : map->GetRoads()) {
+            const auto& start = road.GetStart();
+            const auto& end = road.GetEnd();
+
+            if (road.IsHorizontal()) {
+                roadsJson.push_back(json::object{
+                    {"x0", start.x},
+                    {"y0", start.y},
+                    {"x1", end.x}
+                });
+            } else if (road.IsVertical()) {
+                roadsJson.push_back(json::object{
+                    {"x0", start.x},
+                    {"y0", start.y},
+                    {"y1", end.y}
+                });
+            }
+        }
+        mapJson["roads"] = std::move(roadsJson);
+
+        // Сериализация зданий
+        json::array buildingsJson;
+        for (const auto& building : map->GetBuildings()) {
+            const auto& bounds = building.GetBounds();
+            buildingsJson.push_back(json::object{
+                {"x", bounds.position.x},
+                {"y", bounds.position.y},
+                {"w", bounds.size.width},
+                {"h", bounds.size.height}
+            });
+        }
+        mapJson["buildings"] = std::move(buildingsJson);
+
+        // Сериализация офисов
+        json::array officesJson;
+        for (const auto& office : map->GetOffices()) {
+            const auto& position = office.GetPosition();
+            const auto& offset = office.GetOffset();
+
+            officesJson.push_back(json::object{
+                {"id", *office.GetId()},
+                {"x", position.x},
+                {"y", position.y},
+                {"offsetX", offset.dx},
+                {"offsetY", offset.dy}
+            });
+        }
+        mapJson["offices"] = std::move(officesJson);
+
+        // Отправка ответа
+        send(MakeStringResponse(http::status::ok, json::serialize(mapJson), req.version(), req.keep_alive(), ContentType::APP_JSON));
     }
 
-    // Отправляет ошибку 404 Not Found
-    template <typename Body, typename Allocator, typename Send>
-    void SendNotFound(
-        const http::request<Body, http::basic_fields<Allocator>>& req,
-        Send&& send,
-        const std::string& message
-    ) {
-        json::value error = {
-            {"code", "mapNotFound"},
-            {"message", message}
-        };
-
-        http::response<http::string_body> res{http::status::not_found, req.version()};
-        res.set(http::field::content_type, "application/json");
-        res.body() = json::serialize(error);
-        res.prepare_payload();
-        send(std::move(res));
+    StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
+                                      bool keep_alive, std::string_view content_type = ContentType::TEXT_HTML) {
+        StringResponse response(status, http_version);
+        response.set(http::field::content_type, content_type);
+        response.body() = body;
+        response.prepare_payload();
+        response.keep_alive(keep_alive);
+        return response;
     }
 
-    // Отправляет ошибку 400 Bad Request
-    template <typename Body, typename Allocator, typename Send>
-    void SendBadRequest(
-        const http::request<Body, http::basic_fields<Allocator>>& req,
-        Send&& send,
-        const std::string& message
-    ) {
+    StringResponse createErrorResponse(http::status status, std::string_view code, std::string_view message) {
         json::value error = {
-            {"code", "badRequest"},
+            {"code", code},
             {"message", message}
         };
-
-        http::response<http::string_body> res{http::status::bad_request, req.version()};
-        res.set(http::field::content_type, "application/json");
-        res.body() = json::serialize(error);
-        res.prepare_payload();
-        send(std::move(res));
+        return MakeStringResponse(status, json::serialize(error), 11, false, ContentType::APP_JSON);
     }
 };
 
 }  // namespace http_handler
+
